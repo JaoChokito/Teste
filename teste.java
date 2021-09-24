e0fc0775baebe8401e449269eb524d433a651574
JaoChokito <85703067+JaoChokito@users.noreply.github.com>
2021-09-23 22:24:47 -0300

    primeira leitura de valor com git

--- teste.java
+++ teste.java
@@ -1,1 +1,14 @@
+java.util.Scanner;
 
+public class teste{
+
+    public static void main(String[] args){
+        
+        Scanner ent =new Scanner (System.in);
+        int valor
+        System.out.println("Primeira leitura de valor no git");
+        valor = ent.nextInt();
+
+    }
+
+}
